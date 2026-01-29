f500a1cf6fe81e37401261d1cc837b59a517a1aa
codeleaded <gnu.c.reaper@gmail.com>
2026-01-29 21:39:31 +0100

    updated

--- src/Main.c
+++ src/Main.c
@@ -3,15 +3,15 @@
 int main(int argc,char **argv){
     VCtl vc = VCtl_New("vc","vc/.vcignore");
     
-    //VCtl_Init(&vc);
-    //Files_Create("vc/src/Header0.h");
-    //VCtl_MakeBranch(&vc,"root","main","main got added!");
-    //Files_Create("vc/src/Header1.h");
-    //VCtl_UpdateBranch(&vc,"main","main got updated!");
-    //VCtl_WriteBranches(&vc);
+    VCtl_Init(&vc);
+    Files_Create("vc/src/Header0.h");
+    VCtl_MakeBranch(&vc,"root","main","main got added!");
+    Files_Create("vc/src/Header1.h");
+    VCtl_UpdateBranch(&vc,"main","main got updated!");
+    VCtl_WriteBranches(&vc);
 
-    VCtl_ReadBranches(&vc);
-    VCtl_BackBranch(&vc,"main");
+    //VCtl_ReadBranches(&vc);
+    //VCtl_BackBranch(&vc,"main");
     
     VCtl_Print(&vc);
     VCtl_Free(&vc);
