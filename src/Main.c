2b375120145ecd2273698d442738b207ab79f3c0
codeleaded <gnu.c.reaper@gmail.com>
2026-01-31 03:10:48 +0100

    updated

--- src/Main.c
+++ src/Main.c
@@ -1,14 +1,30 @@
 #include "/home/codeleaded/System/Static/Library/VCtl.h"
 
 int main(int argc,char **argv){
+    // char text[] = "#include <stdio.h>\nint main(){\n\treturn 0;\n}";
+    // Files_WriteT("vc/src/Main.c",text,sizeof(text) - 1);
+
     VCtl vc = VCtl_New("vc","vc/.vcignore");
+
+    //VCtl_Init(&vc);
     
-    VCtl_Init(&vc);
+    VCtl_ReadBranches(&vc);
     
-    //Files_Create("vc/src/Header0.h");
-    VCtl_UpdateBranch(&vc,"root","root got updated 1!");
-    //Files_Create("vc/src/Header0.h");
-    VCtl_UpdateBranch(&vc,"root","root got updated 2!");
+    // text[39] = '1';
+    // Files_WriteT("vc/src/Main.c",text,sizeof(text) - 1);
+    // VCtl_UpdateBranch(&vc,"root","root got updated 1!");
+    
+    // text[39] = '2';
+    // Files_WriteT("vc/src/Main.c",text,sizeof(text) - 1);
+    // VCtl_UpdateBranch(&vc,"root","root got updated 2!");
+
+    // text[39] = '3';
+    // Files_WriteT("vc/src/Main.c",text,sizeof(text) - 1);
+    // VCtl_UpdateBranch(&vc,"root","root got updated 3!");
+
+    // text[39] = '4';
+    // Files_WriteT("vc/src/Main.c",text,sizeof(text) - 1);
+    // VCtl_UpdateBranch(&vc,"root","root got updated 4!");
 
     //Files_Create("vc/src/Header0.h");
     //VCtl_MakeBranch(&vc,"root","main","main got added!");
@@ -17,7 +33,9 @@ int main(int argc,char **argv){
     //VCtl_WriteBranches(&vc);
 
     //VCtl_ReadBranches(&vc);
-    //VCtl_BackBranch(&vc,"main");
+    VCtl_BackBranch(&vc,"root");
+
+    VCtl_WriteBranches(&vc);
     
     VCtl_Print(&vc);
     VCtl_Free(&vc);
