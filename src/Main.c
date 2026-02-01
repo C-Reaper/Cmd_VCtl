37a1181c6d13268163416bf7013b92391c58e0e9
codeleaded <gnu.c.reaper@gmail.com>
2026-02-01 14:46:48 +0100

    updated

--- src/Main.c
+++ src/Main.c
@@ -3,30 +3,33 @@
 int main(int argc,char **argv){
     char branch[] = "root";
     char newbranch[] = "main";
-    char msg[] = "branch added!";
     
-    char text[] = "aaa\nbbb\nccc";
+    char text[] = "aaa\nbbb";
     Files_WriteT("vc/src/Main.txt",text,sizeof(text) - 1);
 
     VCtl vc = VCtl_New("vc","vc/.vcignore");
     
-    char text0[] = "aab\nbbb\nccc";
+    char text0[] = "aaa\nbbb";
     Files_WriteT("vc/src/Main.txt",text0,sizeof(text0) - 1);
-    VCtl_UpdateBranch(&vc,"root",msg);
+    VCtl_UpdateBranch(&vc,"root","root updated!");
     
-    char text1[] = "aab\nbbc\nccc";
+    char text1[] = "aab\nbbc";
     Files_WriteT("vc/src/Main.txt",text1,sizeof(text1) - 1);
-    VCtl_MakeBranch(&vc,"root","main",msg);
+    VCtl_MakeBranch(&vc,"root","main","main got added!");
 
-    char text2[] = "aab\nbbc\nccd";
+    char text2[] = "aac\nbbd";
     Files_WriteT("vc/src/Main.txt",text2,sizeof(text2) - 1);
-    VCtl_UpdateBranch(&vc,"main",msg);
+    VCtl_UpdateBranch(&vc,"root","root updated again!");
     
-    char text3[] = "aab\nbbd\nccc";
-    Files_WriteT("vc/src/Main.txt",text3,sizeof(text3) - 1);
-    VCtl_UpdateBranch(&vc,"root",msg);
-    
-    VCtl_MergeBranch(&vc,"root","main",msg);
+    VCtl_SetFileKeep(&vc,(VCtl_FileKeep[]){
+        VCtl_FileKeep_New("vc/src/Main.txt",CStrCmp_KeepMap_Make((CStrCmp_Keep[]){
+            CStrCmp_Keep_New(0U,CSTRCMP_KEEP_FIRST),
+            CStrCmp_Keep_New(1U,CSTRCMP_KEEP_SECOND),
+            CStrCmp_Keep_Null()
+        })),
+        VCtl_FileKeep_Null()
+    });
+    VCtl_MergeBranch(&vc,"root","main","merge of root and main!");
     
     //VCtl_ReadBranches(&vc);
     //VCtl_UpdateBranch(&vc,branch,msg);
